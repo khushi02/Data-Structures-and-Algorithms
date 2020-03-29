9a43b4ea7842ced6986d38e7a373b8941bbd2bf6
khushi02 <k9wadhwa@gmail.com>
2020-03-28 22:15:49 -0700

    Update

--- Week 1/percolation/Percolation.java
+++ Week 1/percolation/Percolation.java
@@ -4,26 +4,59 @@
  *  Description:
  **************************************************************************** */
 
-import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.UF;
 
 public class Percolation {
+    int n;
+    int[] objArr = new int[n * n];
+    UF uf = new UF(n);
+
+    private int xyTo1D(int row, int col) {
+        return ((row - 1) * n) + col;
+    }
+
+    private void isValid(int i) {
+        if (i > n || i <= 0) {
+            throw new IllegalArgumentException("Number must be between 1 and N");
+        }
+    }
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException("N must be greater than 0");
         }
-        else {
-            int[] rowArr = new int[n];
-            int[] colArr = new int[n];
+        //int[] rowArr = new int[n];
+        //int[] colArr = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                objArr[xyTo1D(i, j)] = 0;
+            }
         }
     }
 
-    /*
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-    }
+        isValid(row);
+        isValid(col);
 
+        objArr[xyTo1D(row, col)] = 1;
+
+        if (objArr[xyTo1D(row - 1, col)] == 1) {
+            uf.union(xyTo1D(row, col), xyTo1D(row - 1, col));
+        }
+        else if (objArr[xyTo1D(row + 1, col)] == 1) {
+            uf.union(xyTo1D(row, col), xyTo1D(row + 1, col));
+        }
+        else if (objArr[xyTo1D(row, col - 1)] == 1) {
+            uf.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+        }
+        else if (objArr[xyTo1D(row, col + 1)] == 1) {
+            uf.union(xyTo1D(row, col), xyTo1D(row, col + 1));
+        }
+    }
+/*
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
     }
@@ -40,7 +73,7 @@ public class Percolation {
     // does the system percolate?
     public boolean percolates() {
     }
-*/
+
     // test client (optional)
     public static void main(String[] args) {
         int n = StdIn.readInt();
@@ -50,13 +83,7 @@ public class Percolation {
         while (!StdIn.isEmpty()) {
             int row = StdIn.readInt();
             int col = StdIn.readInt();
-
-            if (row > n || row <= 0) {
-                throw new IllegalArgumentException("Row number must be between 0 and N");
-            }
-            else if (col > n || col <= 0) {
-                throw new IllegalArgumentException("Column number must be between 0 and N");
-            }
         }
     }
+     */
 }
