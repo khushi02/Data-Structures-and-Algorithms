3bde2c8a3e229c284d584c0f1099dd2a1d00bfad
khushi02 <k9wadhwa@gmail.com>
2020-03-29 13:42:12 -0700

    Update

--- Week 1/percolation/Percolation.java
+++ Week 1/percolation/Percolation.java
@@ -7,9 +7,23 @@
 import edu.princeton.cs.algs4.UF;
 
 public class Percolation {
-    int n;
-    int[] objArr = new int[n * n];
+    private static int n;
+    int[] objArr = new int[n * n + 2];
     UF uf = new UF(n);
+    int count;
+    int virtTop = 0;
+    int virtBottom = n * n + 1;
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("N must be greater than 0");
+        }
+
+        for (int i = 1; i <= n * n; i++) {
+            objArr[i] = 0;
+        }
+    }
 
     private int xyTo1D(int row, int col) {
         return ((row - 1) * n) + col;
@@ -21,21 +35,6 @@ public class Percolation {
         }
     }
 
-    // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException("N must be greater than 0");
-        }
-        //int[] rowArr = new int[n];
-        //int[] colArr = new int[n];
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < n; j++) {
-                objArr[xyTo1D(i, j)] = 0;
-            }
-        }
-    }
-
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         isValid(row);
@@ -43,47 +42,76 @@ public class Percolation {
 
         objArr[xyTo1D(row, col)] = 1;
 
-        if (objArr[xyTo1D(row - 1, col)] == 1) {
+        if (row == 1) {
+            uf.union(virtTop, xyTo1D(row, col));
+            //objArr[xyTo1D(row, col)] = 2;
+        }
+        if (row == n) {
+            uf.union(virtBottom, xyTo1D(row, col));
+            // objArr[xyTo1D(row, col)] = 2;
+        }
+        if (objArr[xyTo1D(row - 1, col)] == 1 || objArr[xyTo1D(row - 1, col)] == 2) {
             uf.union(xyTo1D(row, col), xyTo1D(row - 1, col));
+            //objArr[xyTo1D(row, col)] = 2;
+            //objArr[xyTo1D(row - 1, col)] = 2;
         }
-        else if (objArr[xyTo1D(row + 1, col)] == 1) {
+        if (objArr[xyTo1D(row + 1, col)] == 1 || objArr[xyTo1D(row + 1, col)] == 2) {
             uf.union(xyTo1D(row, col), xyTo1D(row + 1, col));
+            //objArr[xyTo1D(row, col)] = 2;
+            //objArr[xyTo1D(row + 1, col)] = 2;
         }
-        else if (objArr[xyTo1D(row, col - 1)] == 1) {
+        if (objArr[xyTo1D(row, col - 1)] == 1 || objArr[xyTo1D(row, col - 1)] == 2) {
             uf.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+            //objArr[xyTo1D(row, col)] = 2;
+            //objArr[xyTo1D(row, col - 1)] = 2;
         }
-        else if (objArr[xyTo1D(row, col + 1)] == 1) {
+        if (objArr[xyTo1D(row, col + 1)] == 1 || objArr[xyTo1D(row, col + 1)] == 2) {
             uf.union(xyTo1D(row, col), xyTo1D(row, col + 1));
+            //objArr[xyTo1D(row, col)] = 2;
+            //objArr[xyTo1D(row, col + 1)] = 2;
         }
+
+        count++;
     }
-/*
+
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
+        isValid(row);
+        isValid(col);
+
+        if (objArr[xyTo1D(row, col)] == 1 || objArr[xyTo1D(row, col)] == 2) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-    }
+        isValid(row);
+        isValid(col);
 
+        if (objArr[xyTo1D(row, col)] == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
+        return count;
     }
 
     // does the system percolate?
     public boolean percolates() {
+        return uf.connected(virtTop, virtBottom);
     }
 
     // test client (optional)
     public static void main(String[] args) {
-        int n = StdIn.readInt();
-
-        Percolation perc = new Percolation(n);
-
-        while (!StdIn.isEmpty()) {
-            int row = StdIn.readInt();
-            int col = StdIn.readInt();
-        }
     }
-     */
 }
+
