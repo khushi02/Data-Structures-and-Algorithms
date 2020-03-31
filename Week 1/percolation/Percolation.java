4de4ea5d98544ced40161c291825dd94392c6e91
khushi02 <k9wadhwa@gmail.com>
2020-03-31 10:21:23 -0700

    Update Percolation.java

--- Week 1/percolation/Percolation.java
+++ Week 1/percolation/Percolation.java
@@ -9,7 +9,7 @@ import edu.princeton.cs.algs4.UF;
 public class Percolation {
     private static int n;
     int[] objArr = new int[n * n + 2];
-    UF uf = new UF(n);
+    UF uf = new UF(n * n);
     int count;
     int virtTop = 0;
     int virtBottom = n * n + 1;
@@ -25,10 +25,12 @@ public class Percolation {
         }
     }
 
+    // creates a 1D index for a 2D coordinate
     private int xyTo1D(int row, int col) {
         return ((row - 1) * n) + col;
     }
 
+    // checks to make sure row and col are within bounds
     private void isValid(int i) {
         if (i > n || i <= 0) {
             throw new IllegalArgumentException("Number must be between 1 and N");
@@ -44,31 +46,21 @@ public class Percolation {
 
         if (row == 1) {
             uf.union(virtTop, xyTo1D(row, col));
-            //objArr[xyTo1D(row, col)] = 2;
         }
         if (row == n) {
             uf.union(virtBottom, xyTo1D(row, col));
-            // objArr[xyTo1D(row, col)] = 2;
         }
-        if (objArr[xyTo1D(row - 1, col)] == 1 || objArr[xyTo1D(row - 1, col)] == 2) {
+        if (objArr[xyTo1D(row - 1, col)] == 1) {
             uf.union(xyTo1D(row, col), xyTo1D(row - 1, col));
-            //objArr[xyTo1D(row, col)] = 2;
-            //objArr[xyTo1D(row - 1, col)] = 2;
         }
-        if (objArr[xyTo1D(row + 1, col)] == 1 || objArr[xyTo1D(row + 1, col)] == 2) {
+        if (objArr[xyTo1D(row + 1, col)] == 1) {
             uf.union(xyTo1D(row, col), xyTo1D(row + 1, col));
-            //objArr[xyTo1D(row, col)] = 2;
-            //objArr[xyTo1D(row + 1, col)] = 2;
         }
-        if (objArr[xyTo1D(row, col - 1)] == 1 || objArr[xyTo1D(row, col - 1)] == 2) {
+        if (objArr[xyTo1D(row, col - 1)] == 1) {
             uf.union(xyTo1D(row, col), xyTo1D(row, col - 1));
-            //objArr[xyTo1D(row, col)] = 2;
-            //objArr[xyTo1D(row, col - 1)] = 2;
         }
-        if (objArr[xyTo1D(row, col + 1)] == 1 || objArr[xyTo1D(row, col + 1)] == 2) {
+        if (objArr[xyTo1D(row, col + 1)] == 1) {
             uf.union(xyTo1D(row, col), xyTo1D(row, col + 1));
-            //objArr[xyTo1D(row, col)] = 2;
-            //objArr[xyTo1D(row, col + 1)] = 2;
         }
 
         count++;
@@ -79,7 +71,7 @@ public class Percolation {
         isValid(row);
         isValid(col);
 
-        if (objArr[xyTo1D(row, col)] == 1 || objArr[xyTo1D(row, col)] == 2) {
+        if (objArr[xyTo1D(row, col)] == 1) {
             return true;
         }
         else {
